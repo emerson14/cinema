821c90846646c62656d6b3df20e5093abed592ed
Rael <elias-toro@outlook.com>
2022-07-14 22:07:36 -0500

    add user logic was added, waiting for the frontend form to be ready

--- js/main.js
+++ js/main.js
@@ -3,11 +3,52 @@
 var app = new Vue({
     el: '#app',
     data: {
-        
+        users: [],
+        newUser: {
+            name: 'test',
+            lastName: 'test1',
+            address: 'asdfas',
+            phoneNumber: '1234',
+            email: 'a@algo.com',
+            password: '1234',
+            status: 'inactive',
+        },
+        confirmPass: '',//para el input de confirmación de contraseña
+
     },
     methods: {//metodos a trabajar en el proyecto -- VARIABLES EN camelCase
-        abc(){
-            
+        addUser(){
+            if (
+                this.newUser.name.length > 0 && this.newUser.lastName.length > 0 
+                && this.newUser.address.length > 0 && this.newUser.phoneNumber.length > 0 &&
+                this.newUser.email.length > 0 && this.newUser.password.length > 0
+                ) {
+                    if (this.confirmpass === this.newUser.password) {
+                        this.users.push({
+                            ...this.newUser
+                        });
+                        this.clearFields();
+                        //this.updateLocalStorage();
+                        this.mensaje("Su cuenta ha sido creada, un código de valicación fue enviado a su correo", "success");
+                    }else{
+                        this.mensaje("La contraseña ingresada y la confirmación no coinciden", "error");
+                    }
+                
+            }else{
+                this.mensaje("Por favor rellene todos los campos", "error");
+            }
+        },
+        clearFields(){//limpiará los campos del formulario de registro de usuarios al crear la cuenta
+            this.newUser = {
+                name: '',
+                lastName: '',
+                address: '',
+                phoneNumber: '',
+                email: '',
+                password: '',
+                status: 'inactive',
+            }
+            this.confirmpass = '';
         },
         mensaje: function (msj, icono) {//para enviar alerts de sweet alert
             const Toast = Swal.mixin({
@@ -21,7 +62,6 @@ var app = new Vue({
                     toast.addEventListener('mouseleave', Swal.resumeTimer)
                 }
             })
-
             Toast.fire({
                 icon: icono,
                 title: msj
