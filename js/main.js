6516e4e09bb2156577ae24eb39fe741c550b840c
Rael <elias-toro@outlook.com>
2022-07-19 13:12:27 -0500

    buy ticket and chairs selection were updated

--- js/main.js
+++ js/main.js
@@ -12,10 +12,9 @@ var app = new Vue({
                 email: 'admin@algo.com',
                 password: '1234',
                 role: 'superAdmin',
-                status: 'inactive',
+                status: 'active',
             },
         ],
-
         newUser: {
             name: '',
             lastName: '',
@@ -63,6 +62,20 @@ var app = new Vue({
             img: '../img/thorAmorYTruenoCard.png'
             }
         ],
+        rooms: [
+            {roomCode: 'A1', chairs: [
+            { number: 1, status: 'available' },
+            { number: 2, status: 'available' },
+            { number: 3, status: 'available' }, 
+            { number: 4, status: 'available' }
+        ],},
+            {roomCode: 'A2', chairs: [
+            { number: 1, status: 'available' },
+            { number: 2, status: 'available' },
+            { number: 3, status: 'available' }, 
+            { number: 4, status: 'available' }
+        ],}
+        ],
         confirmPass: '',//para el input de confirmación de contraseña
         email: '',
         password: '',
@@ -72,7 +85,11 @@ var app = new Vue({
         genCode: '',
         startTimer: '',
         seconds: 30,
-
+        option: '',
+        rpos: 0,
+        openPayment: false,
+        nTickets: 0,
+        pMethod: '',
     },
     methods: {//metodos a trabajar en el proyecto -- VARIABLES EN camelCase
         addUser(){
@@ -169,6 +186,33 @@ var app = new Vue({
             }
 
         },
+        logout(){
+            const swalWithBootstrapButtons = Swal.mixin({
+                customClass: {
+                    confirmButton: 'btn btn-success',
+                    cancelButton: 'btn btn-danger'
+                },
+                buttonsStyling: false
+            })       
+            swalWithBootstrapButtons.fire({
+                title: '¿Seguro que desea cerrar sesión?',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Sí, cerrar sesión',
+                cancelButtonText: 'Cancelar',
+                reverseButtons: true
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    this.user = '';
+                    this.updateLocalStorage();
+                    this.mensaje("Se ha cerrado la sesión", "success");
+                    setTimeout(function(){ location.href = "login.html" }, 1500);
+                } else if (
+                    result.dismiss === Swal.DismissReason.cancel
+                ) {
+                }
+            })
+        },
         timer(){
             
             if(this.seconds != 0){
@@ -223,7 +267,53 @@ var app = new Vue({
 
 
 
-
+        getRoomIndex(){
+            const index = this.rooms.findIndex((object) => {
+                return object.roomCode == this.option;
+            });
+            this.rpos = index;
+        },
+        chairStatus(item, index){
+            if (this.nTickets > 0) {
+                this.nTickets -= 1;
+                item.status = 'unavaliable';
+                this.chairPos = index;
+                if (this.nTickets == 0) {
+                    this.mensaje('Ha alcanzado la cantidad máxima de puestos según sus tickets', 'warning');
+                    setTimeout(() => { this.mensaje('Por favor haga click en comprar', 'warning'); }, 2500);
+                }
+            }
+        },
+        saveChairStatus(){
+            if (this.pMethod.length > 0) {
+                this.mensaje('Pago exitoso, gracias por preferirnos!', 'success');
+                this.option = '';
+                this.openPayment = false;
+                this.pMethod = '',
+                this.updateLocalStorage();
+            }else{
+                this.mensaje('Por favor seleccione un método de pago', 'error');
+            }
+            
+        },
+        goToPay(){
+            if (this.nTickets == 0) {
+                switch (this.openPayment) {
+                    case true:
+                        this.openPayment = false;
+                        break;
+                    case false:
+                        this.openPayment = true;
+                        break;
+                
+                    default:
+                        break;
+                }
+            }else{
+                this.mensaje('Seleccione los puestos', 'warning');
+            }
+            
+        },
         mensaje: function (msj, icono) {//para enviar alerts de sweet alert
             const Toast = Swal.mixin({
                 toast: true,
@@ -244,6 +334,7 @@ var app = new Vue({
         updateLocalStorage(){
             localStorage.setItem('users', JSON.stringify(this.users));
             localStorage.setItem('user', JSON.stringify(this.user));
+            localStorage.setItem('rooms', JSON.stringify(this.rooms));
         },
 
     },
@@ -257,6 +348,11 @@ var app = new Vue({
             this.user = JSON.parse(localStorage.getItem('user'));
         }else{
             this.user = '';
+        }
+        if (localStorage.getItem('rooms') !== null) {
+            this.rooms = JSON.parse(localStorage.getItem('rooms'));
+        }else{
+            this.rooms = this.rooms;
         }
     }
 });
